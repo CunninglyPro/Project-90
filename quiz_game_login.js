edf45aaa437e3095e7595d1aaba95210c7296e69
Aditya <99547519+CunninglyPro@users.noreply.github.com>
2022-12-13 16:11:57 +0530

    Delete quiz_game_login.js

--- quiz_game_login.js
+++
@@ -1,9 +0,0 @@
-function addUser() {
-    var player_1 = document.getElementById("playerlogin1").value;
-    var player_2 = document.getElementById("playerlogin2").value;
-
-    localStorage.setItem("player1login", player_1);
-    localStorage.setItem("player2login", player_2);
-
-    window.location = "quiz_game_page.html";
-}   
